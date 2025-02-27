47f08ed45371890892005420f102c8c8fe5b0d7a
Pedro Oliveira <ph30083@gmail.com>
2025-02-27 20:41:45 -0300

    exercicios feitos em aula

---
+++ Aula01_24_02_25.py
@@ -0,0 +1,32 @@
+# # criando uma calculadora para calcular a area de um triangulo
+# print("Bem vindo á calculadora de area de triangulo")
+# base = int(input("Insira a medida da base do triangulo em cm: "))
+# altura = int(input("Insira a medida da altura do triangulo em cm: "))
+# # area = (base*altura)/2
+# print("A area do triangulo inserido é de ", (base*altura)/2, " cm²")
+
+# # criando uma calculadora de perimetro de triangulos
+# print("Bem vindo á calcaladora de perimetro de triangulos")
+# lado1 = float(input("Insira a medida do primeiro lado em cm: "))
+# lado2 = float(input("insira a medida do segundo lado em cm: "))
+# lado3 = float(input("insira a medida do sterceiro lado do triangulo: "))
+# # perimetro = (lado1+lado2+lado3)
+# print("o perimetro do triangulo é ", (lado1+lado2+lado1))
+
+
+# # cirando uma calculadora paracalculo de area e perimetro de quadrado equilatero
+# print("Bem vindo a calculadora de area e perimetro de quadrados equilateros ")
+# lateral = float(input("insira a medida do lado do quadrado em cm: "))
+# # perimetro = lateral*4
+# # area = lateral**2
+# print("o perimetro do quadrado mede ", (lateral*4),
+#       "cm e sua area é de ", (lateral**2))
+
+# # criando uma calculadora para calculo de area e perimetro de retangulos
+# print("Bem vindo a calculadora de area e perimetro de retangulos ")
+# base = float(input("insira a medida da base do retangulo em cm: "))
+# altura = float(input("insira a medida da altura do retangulo em cm: "))
+# # perimetro=(base*2)+(altura*2)
+# # area =(base * altura)
+# print("o retangulo inserido possui um perimetro de ", (base*2) +
+#       (altura*2), " cm e uma area de ", (base * altura), " cm")
