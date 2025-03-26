8789af1d009e5a5c897551e38905a0e969517957
Pedro Oliveira <ph30083@gmail.com>
2025-03-26 09:10:33 -0300

    segundo exercicio - conversão de c p/ f

--- Lista_de_exercicios_01.py
+++ Lista_de_exercicios_01.py
@@ -1,14 +1,32 @@
+# EXERCICIO 1
 # Faça um programa em python que converte temperaturas expressas em graus Farenheit para graus
 # celsius. seu programa deve solicitar a digitação do valor convertido (F). A relação entre gruas
 # celsius e graus farenheit é c = 5/(f-32)
 
-print("Programa para conversão de te temperatura C° p/ F° ")
+# print("Programa para conversão de te temperatura F° p/ C° ")
+
+# try:
+#     tempF = float(
+#         input("Insira a temperatura em Farenheit: "))
+# except ValueError:
+#     print("O VALORR DIGITADO DEVE SER UM NUMERO!!")
+# else:
+#     print("A temepratura digitada em graus Farenheit foi de ", tempF,
+#           " e seu valor correspondente em graus Celsius é ", (5/9*(tempF-32)))
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+# EXERCICIO2
+# Faça um programa que converte temperaturas expressas em gruas celcius para gruas farenheit.Seu
+# programa deve solicitar a digitação do valor a ser convertido (c). A relação entre graus celcius e graus farenheit é c=5/9(f-32)
+
+print("Programa para conversão de temperatura F° p/ C°")
 
 try:
-    tempF = float(
-        input("Insira a temperatura em Farenheit: "))
+    tempc = float(input("Insira a temperatura em Celsius"))
 except ValueError:
-    print("O VALORR DIGITADO DEVE SER UM NUMERO!!")
+    print("O valor inserido deve ser um numero!!")
 else:
-    print("A temepratura digitada em graus Farenheit foi de ", tempF,
-          " e seu valor correspondente em graus Celsius é ", (5/9*(tempF-32)))
+    print("A tempertura inserida em graus celsius foi de ", tempc,
+          " e seu valor correspondente em gruas farenheit é ", (tempc*(9/5))+32)
