bb8e0533b508d782078f85e524395b14fc982bdd
Pedro Oliveira <ph30083@gmail.com>
2025-03-26 11:07:46 -0300

    salvando

--- Lista_de_exercicios_01.py
+++ Lista_de_exercicios_01.py
@@ -69,25 +69,96 @@
 # Faça um programa em pythin que converte temeperaturas expressas em graus celsius para graus rankine. seu programa
 # deve solicitar a digitação do valor a ser convertido (C). A relação entre graus celsius e graus rankine é c=(r/1.8)-491,97
 
-print("Programa para conversão de graus celsius rankine para graus celsius")
+# print("Programa para conversão de graus celsius rankine para graus celsius")
 
-try:
-    tempr = float(input("Insira o valor da temperatura em graus Rankine: "))
-except ValueError:
-    print("O valor inserido deve ser um numero!!!")
-else:
-    print('O valor inserido em graus Rankine foi de ', tempr,
-          " e seu valor correspondente em graus celcius é ", (tempr-491.67)*5/9)
-
-# <--------------------------------------------------------------------------------------------------------------->
-# EXERCICIO
-
+# try:
+#     tempc = float(input("Insira o valor da temperatura em graus Rankine: "))
+# except ValueError:
+#     print("O valor inserido deve ser um numero!!!")
+# else:
+#     print('O valor inserido em graus Rankine foi de ', tempc,
+#           " e seu valor correspondente em graus celcius é ", (tempc-491.67)*5/9)
 
 # <--------------------------------------------------------------------------------------------------------------->
-# EXERCICIO
+# EXERCICIO6
+# Faça um programa que converte temperaturas expressas em graus rankine para graus celsius.Seu programa deve
+# solicitar a digitação do valor a ser convertido (c). A relação entre graus rankine é c=(r/1.8)-491.67
 
+# print("Programa para conversão de graus Rankine para graus Celsius")
+
+# try:
+#     tempr = float(input("Insira o valor da temperatura em grua rankine: "))
+# except ValueError:
+#     print("O valor inserido deve ser um nuemro!!")
+# else:
+#     print("O valor digitado em graus rankine foi de ", tempr,
+#           " e seu valor correspondente em graus celsius é de ", (tempr-491.67)*5/9)
 
 # <--------------------------------------------------------------------------------------------------------------->
+# EXERCICIO7
+# Levando em conta as relações entre unidade de temperatura mostradas nos 6 primeiros exercicos. Faça um progrma em python
+# que converte temperaturas expressas em graus kelvin para graus farenheit. Seu programa deve solicitar a digitação do valor a ser convertido (k).
+
+# print("Programa para a conversão de graus kelvin para graus farenheit")
+
+# try:
+#     tempk = float(input("Insira o valor da temperatura em graus Kelvin: "))
+# except ValueError:
+#     print("O valor inserido deve ser um numero!!")
+# else:
+#     print("O valor da temperatura em graus Kelvin ", tempk,
+#           " e seu valor correspondente em graus farenheit é de ", (tempk - 273.15)*9/5+32)
+
+# <--------------------------------------------------------------------------------------------------------------->
+# EXERCICIO8
+# Levando em conta as unidades de temperatura mostradas nos 6 primeiros exercicios, faça um programa em python que converte
+# temperaturas expressas em graus farenheit para graus kelvin. seu programa deve solicitar a digitação do valor a ser convertido (f)
+
+# print("Programa de conversão de graus farenheit para kelvin ")
+
+# try:
+#     tempf = float(input("Insira o valor da temperatura em gruas Farenheit: "))
+# except ValueError:
+#     print("O valor digitado deve ser um numero!!")
+# else:
+#     print("O valor de temperatura em graus farenheit ", tempf,
+#           " e seu valor coreespondente em graus kelvin é de ", (tempf-32)*5/9+273.15)
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+# # EXERCICIO9
+# Levando em conta as relações entre unidades de temperatura mostrada nos 6 primeiros exercicios, faça um programa em python
+# que converte temperaturas expressas em graus farenheit para graus rankine. Seu programa deve solicitar a digitação do valor a ser convertido(f).
+
+# print((" Programa para a conversão de graus Farenheit para graus Rankine"))
+
+# try:
+#     tempf = float(input("Insira o valor de temperatura em graus Farenheit: "))
+# except ValueError:
+#     print("O valor digitado ve ser numerico!!")
+# else:
+#     print("O valor de temperatura em graus Farenheit ", tempf,
+#           " e seu valor correspondente em graus Rankine é de ", tempf+459.67)
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+# EXERCICIO10
+# Levando em conta as relações entre unidades de temperatura mostradas nos 6 primeiros exercicios,faça um
+# programa em python que converte temperaturas expressas em graus rankine para graus farenheit. Seu programa
+# deve solicitar a digitação do valor a ser convertido (r)
+
+# print("Programa para a conversão de graus Rankine para graus Farenheit! ")
+
+# try:
+#     tempr = float(input("Insira o valor da temperatura em graus Rankine: "))
+# except ValueError:
+#     print("O valor inserido deve ser numerico!! ")
+# else:
+#     print("O valor de temperatura em graus rankine ", tempr,
+#           " e seu valor correspondente em graus farenheit é de ", tempr-459.67)
+
+# <--------------------------------------------------------------------------------------------------------------->
+
 # EXERCICIO
 
 
