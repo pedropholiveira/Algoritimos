fae48a5a4f08e560956696c68053fb89898fef6a
Pedro Oliveira <ph30083@gmail.com>
2025-03-26 09:20:04 -0300

    conversão de c para k

--- Lista_de_exercicios_01.py
+++ Lista_de_exercicios_01.py
@@ -3,7 +3,7 @@
 # celsius. seu programa deve solicitar a digitação do valor convertido (F). A relação entre gruas
 # celsius e graus farenheit é c = 5/(f-32)
 
-# print("Programa para conversão de te temperatura F° p/ C° ")
+# print("Programa para conversão de te temperatura F° p/ C°")
 
 # try:
 #     tempF = float(
@@ -21,12 +21,52 @@
 # Faça um programa que converte temperaturas expressas em gruas celcius para gruas farenheit.Seu
 # programa deve solicitar a digitação do valor a ser convertido (c). A relação entre graus celcius e graus farenheit é c=5/9(f-32)
 
-print("Programa para conversão de temperatura F° p/ C°")
+# print("Programa para conversão de temperatura F° p/ C°")
+
+# try:
+#     tempc = float(input("Insira a temperatura em Celsius: "))
+# except ValueError:
+#     print("O valor inserido deve ser um numero!!")
+# else:
+#     print("A tempertura inserida em graus celsius foi de ", tempc,
+#           " e seu valor correspondente em gruas farenheit é ", (tempc*(9/5))+32)
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+# EXERCICIO3
+# Faça um programa em python que converte temperaturas expressas em graus celcius para graus kelvin. Seu programa deve solicitar a digitação
+# do valor a ser convertido (c). A relação entre graus celsius e grau kelvin é c= k-273,15
+
+print("Programa para a conversão de temperaturas C° p/ K° ")
 
 try:
-    tempc = float(input("Insira a temperatura em Celsius"))
+    tempc = float(input("Insira o valor da temperatura em graus Celsius: "))
 except ValueError:
-    print("O valor inserido deve ser um numero!!")
+    print('O valor inserido deve ser um numero!! ')
 else:
-    print("A tempertura inserida em graus celsius foi de ", tempc,
-          " e seu valor correspondente em gruas farenheit é ", (tempc*(9/5))+32)
+    print("A temperatura inserida em gruas celsius é ", tempc,
+          " e seu valor correspondente em graus kelvin é ", (tempc+273.15))
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
