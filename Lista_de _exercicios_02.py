bf45dc24c43c6a6cb782641fde23b575b9e106c1
Pedro Oliveira <ph30083@gmail.com>
2025-03-27 15:19:35 -0300

    iniciando a segunda lista

---
+++ Lista_de _exercicios_02.py
@@ -0,0 +1,30 @@
+# Lista de exercicios02
+
+# Melhorias
+
+# Exercicios de 1 a 27
+# Refaça os exercicios de 1 a 27 da primeira lista de exercicios
+# (exercicios de sequencia), agora validando as digitações dos usuarios.
+# Para tanto, alem das peculiaridades que cada exercicio possa ter, lembre-se
+# que temperaturas não podem ser inferiores ao zero absoluto (zero graus kelvin).
+# Lembre-se tambem que medidas tomadas de figuras geometricas jamais poderão ser negativas e nem zero.
+
+# EXERCICIO28
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
+
+# EXERCICIO
+
+
+# <--------------------------------------------------------------------------------------------------------------->
